15fac84a94288d34b66ff0d503f892408665e72e
Ka1beatsCN <huangkai19912008@163.com>
2025-12-03 16:50:42 +0800

    Update onion.js

--- some interesting codes/onion.js
+++ some interesting codes/onion.js
@@ -1,1 +1,22 @@
+function composer(functions) {
+  function helper(i) {
+    if(i < functions.length - 1) {
+      return () => functions[i](helper(i+1))
+    }
+    return () => functions[i](() => {})
+  }
+  return helper(0)
+}
 
+// use generator to compose onion
+function composer2(functions){
+  let next;
+  let gen = function* (){
+      yield* functions
+  }
+  let gg = gen()
+
+  next = () => gg.next().value?.(next)
+
+  return next
+}
