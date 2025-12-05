3db8c031bc94f6faac31132d0fdfb6fdc35775ec
Ka1beatsCN <huangkai19912008@163.com>
2025-12-05 12:01:06 +0800

    Update applyCallBindNew.js

--- some interesting codes/applyCallBindNew.js
+++ some interesting codes/applyCallBindNew.js
@@ -1,1 +1,36 @@
+// apply
+Function.prototype.myApply = function(tar, args = []) {
+  // number boolean string symbol
+  const typeSet = new Set(['number', 'boolean', 'string', 'symbol'])
+  if(typeSet.has(typeof args)) {
+    throw new TypeError('CreateListFromArrayLike called on non-object')
+  }
+  const sbl = Symbol()
+  
+  tar = Object(tar)
+  tar[sbl] = this
+  args = Array.isArray(args) ? args : []
+  const res = tar[sbl](...args)
+  
+  delete tar[sbl]
+  return res
+}
+
+// call
+Function.prototype.myCall = function(tar, ...args) {
+  const sbl = Symbol()
+
+  tar = Object(tar)
+  tar[sbl] = this
+  const res = tar[sbl](...args)
+  
+  delete tar[sbl]
+  return res
+}
+
+// bind
+
+
+// new
+
 
