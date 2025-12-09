30bb6d39d178556867e521487d2c56f526007173
Ka1beatsCN <huangkai19912008@163.com>
2025-12-09 19:32:57 +0800

    Update await.js

--- some interesting codes/await.js
+++ some interesting codes/await.js
@@ -1,1 +1,39 @@
+function genAsync(genf) {
+  return function(...args) {
+    const gen = genf.call(this, ...args)
 
+    return new Promise((res, rej) => {
+      function step(key, arg) {
+        let genRes
+        try {
+          genRes = gen[key][arg]
+        } catch(err) {
+          return rej(err)
+        }
+
+        const {val, done} = genRes
+
+        if(done) {
+          return res(val)
+        } 
+        return Promise.resolve(val).then(v => step['next'](val), err => step['throw'](err))
+      }
+    
+      step('next')
+    })
+  }
+}
+/*
+  // compiler change async to generator
+  function * test() { // async test() {
+    const a = yield asyncFunc() // const a = await asyncFunc()
+    {}
+    const b = yield asyncFunc() // const b = await asyncFunc()
+    {}
+    const c = yield asyncFunc() // const c = await asyncFunc()
+  }
+  
+  const gen = genAsync(test)
+  gen().then(...)
+*/
+  
